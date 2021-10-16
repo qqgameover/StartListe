7a773f6b3bc124296b468601154d88b586b3eb28
qqgameover <kasper35@live.no>
2021-10-16 17:59:10 +0200

    Initial commit

---
+++ StartListe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StartListe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
